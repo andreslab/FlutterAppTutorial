46d45e3859fe97cc19f788c7b46a18d09fba12ba
Jaime Andrade <jaimeandrade454@gmail.com>
2019-10-18 06:29:35 -0500

    init

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'pages/login.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "Flutter Demo",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: LoginPage(),
+    );
+  }
+}
